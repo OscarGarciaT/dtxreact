8d432ac09af1dc12aa5f2f221652e26ab782a835
Andrea Velez <avelez@comextweb.com>
2023-07-29 22:10:44 -0500

    Se agrega componente de calendario

--- src/Pages/Calendario.jsx
+++ src/Pages/Calendario.jsx
@@ -4,9 +4,18 @@ import AddIcon from '@mui/icons-material/Add';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { useDispatch } from "react-redux";
 import { pushDialog } from "../slices/dialogSlice";
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
+import '/src/index.css';
 
 const Calendario = () => {
-
+    const data = [
+        {
+          Id: 1,
+          Subject: 'Meeting',
+          StartTime: new Date(2023, 1, 15, 10, 0),
+          EndTime: new Date(2023, 1, 15, 12, 30),
+        },
+      ];
     //Renderizar modal de appointment
     const dispatch = useDispatch();
     const handleCrearNuevaCita = () => {
@@ -23,7 +32,16 @@ const Calendario = () => {
                 </Typography>
                 <Button variant="contained" onClick={handleCrearNuevaCita}>Nueva Cita<AddIcon /></Button>
             </div>
-
+            <div>
+                <ScheduleComponent
+                    selectedDate={new Date(2023, 1, 15)}
+                    eventSettings={{
+                        dataSource: data,
+                    }}
+                >
+                    <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+                </ScheduleComponent>
+            </div>
         </div>
     )
 }
