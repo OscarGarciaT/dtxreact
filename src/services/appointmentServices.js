398ee9206a39d643361c2c15067b07c6fa7ae24b
Eduardo Salavarria <eesalava@espol.edu.ec>
2023-08-11 17:48:10 -0500

    fix(services): cambio en la ruta del servicio API

--- src/services/appointmentServices.js
+++ src/services/appointmentServices.js
@@ -2,12 +2,12 @@ import { createRequest } from "../utils/requestUtils";
 
 export const createAppointment  = (doctorId, appointmentData) => {
     return createRequest()
-        .post(`patients/${doctorId}/create_appointment`, {appointmentData })
+        .post(`appointments/${doctorId}/create_appointment`, {appointmentData })
         .then((res) => res.data);
 };
 
 export const getAppointments = (doctorId) => {
     return createRequest()
-      .get(`patients/${doctorId}/appointments`)
+      .get(`appointments/${doctorId}/all`)
       .then((res) => res.data);
   };
