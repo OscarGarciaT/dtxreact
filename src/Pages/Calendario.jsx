42dc4fc7852f8b8bafe591764f6cbe37170b3b7c
Andrea Velez <avelez@comextweb.com>
2023-08-10 19:33:10 -0500

    Fix calendario

--- src/Pages/Calendario.jsx
+++ src/Pages/Calendario.jsx
@@ -4,19 +4,29 @@ import AddIcon from '@mui/icons-material/Add';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { useDispatch } from "react-redux";
 import { pushDialog } from "../slices/dialogSlice";
-import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
-import '/src/index.css';
 
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import useAppointments from '../utils/hooks/useAppointments';
 
-//import { getAppointments } from "../services/appointmentServices";
 
 const Calendario = () => {
-    const events = [
-        { title: 'Meeting', start: new Date() },
-    ];
+    const appointments = useAppointments();
+    const setEvents = () => {
+      let array = []
+      for (const cita of appointments){
+        let newCita = {title:'', start:'', end:''}
+        newCita.end = Date.parse(cita.hora_fin_cita)
+        newCita.start = Date.parse(cita.hora_inicio_cita)
+        newCita.title = cita.motivo
+        array.push(newCita)
+      }
+      console.log("hola")
+      console.log(array)
+      return array
+    }
+    const events = setEvents()
 
     function renderEventContent(eventInfo) {
       return (
@@ -33,7 +43,7 @@ const Calendario = () => {
             pushDialog({ id: "CREATE_APPOINTMENT_VIEW", props: { mode: "CREATE" } })
         );
     }
-
+    
     return (
         <div className="calendario-containter p-10">
             <div className="calendario-header flex flex-row justify-between">
