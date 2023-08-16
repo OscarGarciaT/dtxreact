c9d67856213263e9ee85714a66c5bb5742683c10
Oscar Garcia Tinoco <84405742+OscarGarciaT@users.noreply.github.com>
2023-08-15 20:00:52 -0500

    wip drawing and deletion

--- src/Components/Odontograma.jsx
+++ src/Components/Odontograma.jsx
@@ -1,5 +1,6 @@
 import {
   Autocomplete,
+  Box,
   Button,
   Icon,
   IconButton,
@@ -31,15 +32,33 @@ const simbolos = [
   { label: "Protesis", value: "protesis.png" },
 ];
 
-const OdontogramaCanvas = (props) => {
+const colors = [
+  { label: "Negro", value: "black" },
+  { label: "Rojo", value: "red" },
+  { label: "Azul", value: "blue" },
+];
+
+const brushSizes = [
+  { label: "Pequeño", value: 1 },
+  { label: "Mediano", value: 2 },
+  { label: "Grande", value: 3 },
+];
+
+const deleteMargin = 25;
+
+const OdontogramaCanvas = ({
+  placedSymbols,
+  drawings,
+  editingMode,
+  mousePosition,
+  ...props
+}) => {
   const canvasRef = useRef(null);
   const backgroundImage = "/assets/img/odontograma_canvas.png";
-  const placedSymbols = props.placedSymbols;
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-
     const img = new Image();
     img.src = backgroundImage;
     img.onload = () => {
@@ -54,19 +73,112 @@ const OdontogramaCanvas = (props) => {
           ctx.drawImage(symbolImg, x, y);
         };
       });
+
+      drawings.forEach((drawing) => {
+        ctx.beginPath();
+        ctx.moveTo(drawing.startX, drawing.startY);
+        ctx.lineTo(drawing.endX, drawing.endY);
+        ctx.strokeStyle = drawing.color;
+        ctx.lineWidth = drawing.thickness;
+        ctx.stroke();
+      });
+
+      if (editingMode === "deleting" && mousePosition.holding) {
+        ctx.beginPath();
+        ctx.rect(
+          mousePosition.x - deleteMargin,
+          mousePosition.y - deleteMargin,
+          2 * deleteMargin,
+          2 * deleteMargin
+        );
+        ctx.strokeStyle = "red";
+        ctx.stroke();
+      }
     };
-  }, [backgroundImage, placedSymbols]);
+  }, [backgroundImage, placedSymbols, drawings, mousePosition, editingMode]);
 
   return <canvas ref={canvasRef} width={1200} height={600} {...props} />;
 };
 
-const Odontograma = (props) => {
-  const [selectedSymbol, setSelectedSymbol] = useState(null);
+// TODO: En el Canvas agregar un recuadro que encierre los dientes circulares
+// TODO: En "Recesion" y "Movilidad" se escriben numeros (grados) del [1,4]
+// TODO: Permitir escoger el tamaño de los simbolos que se van a colocar
+// TODO: Modo de dibujo para anotaciones, con seleccion de color (opcional: tamaño de lapiz)
+// TODO: Guardar coordenadas de los elementos en el canvas
+
+// Vestibular, mesial, lingual, distal
+
+const Odontograma = ({}) => {
   const [placedSymbols, setPlacedSymbols] = useState([]);
-  const [deleteMode, setDeleteMode] = useState(false);
+  const [drawings, setDrawings] = useState([]);
+
+  const [selectedSymbol, setSelectedSymbol] = useState({
+    label: "Extraccion indicada",
+    value: "extraccion_indicada.png",
+  });
+  const [selectedColor, setSelectedColor] = useState({
+    label: "Negro",
+    value: "black",
+  });
+  const [selectedBrushSize, setSelectedBrushSize] = useState({
+    label: "Pequeño",
+    value: 1,
+  });
+
+  const [editingMode, setEditingMode] = useState("adding");
+
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const [lastX, setLastX] = useState(0);
+  const [lastY, setLastY] = useState(0);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (event) => {
+    setIsDrawing(true);
+    setLastX(event.nativeEvent.offsetX);
+    setLastY(event.nativeEvent.offsetY);
+  };
+
+  const handleMouseUp = (event) => {
+    setIsDrawing(false);
+    setMousePosition((prevValue) => ({ ...prevValue, holding: false }));
+  };
+
+  const handleMouseMove = (event) => {
+    if (editingMode === "drawing") {
+      if (!isDrawing) return;
+      const newDrawing = {
+        startX: lastX,
+        startY: lastY,
+        endX: event.nativeEvent.offsetX,
+        endY: event.nativeEvent.offsetY,
+        color: selectedColor ? selectedColor.value : "black",
+        thickness: selectedBrushSize ? selectedBrushSize.value : 1,
+      };
+      setDrawings((prevDrawings) => [...prevDrawings, newDrawing]);
+      setLastX(event.nativeEvent.offsetX);
+      setLastY(event.nativeEvent.offsetY);
+    } else if (editingMode === "deleting") {
+      if (!isDrawing) return;
+      const mouseX = event.nativeEvent.offsetX;
+      const mouseY = event.nativeEvent.offsetY; // adjust this to change the size of the deleting area
+      setMousePosition({ x: mouseX, y: mouseY, holding: true });
+      setDrawings((prevDrawings) =>
+        prevDrawings.filter((drawing) => {
+          // Check if the mouse is over the drawing or within the deleteMargin
+          const isOverDrawing =
+            mouseX >= Math.min(drawing.startX, drawing.endX) - deleteMargin &&
+            mouseX <= Math.max(drawing.startX, drawing.endX) + deleteMargin &&
+            mouseY >= Math.min(drawing.startY, drawing.endY) - deleteMargin &&
+            mouseY <= Math.max(drawing.startY, drawing.endY) + deleteMargin;
+          return !isOverDrawing;
+        })
+      );
+    }
+  };
 
   const handleCanvasClick = async (event) => {
-    if (deleteMode) {
+    if (editingMode === "deleting") {
       const canvas = event.target;
       const canvasRect = canvas.getBoundingClientRect();
       const clickX = event.clientX - canvasRect.left;
@@ -94,7 +206,7 @@ const Odontograma = (props) => {
           break;
         }
       }
-    } else if (selectedSymbol) {
+    } else if (editingMode === "adding" && selectedSymbol) {
       const canvas = event.target;
       const canvasRect = canvas.getBoundingClientRect();
       const x = event.clientX - canvasRect.left;
@@ -113,7 +225,7 @@ const Odontograma = (props) => {
         <Autocomplete
           className="w-1/3"
           value={selectedSymbol}
-          disabled={deleteMode}
+          disabled={editingMode !== "adding"}
           onChange={(event, newValue) => setSelectedSymbol(newValue)}
           options={simbolos}
           getOptionLabel={(option) => option.label}
@@ -158,15 +270,57 @@ const Odontograma = (props) => {
           )}
         />
         <Button
-          onClick={() => setDeleteMode(!deleteMode)}
-          variant={deleteMode ? "contained" : "outlined"}
+          className="mr-5"
+          onClick={() => setEditingMode("adding")}
+          variant={editingMode === "adding" ? "contained" : "outlined"}
+        >
+          <Icon>add</Icon>
+        </Button>
+        <Button
+          onClick={() => setEditingMode("drawing")}
+          variant={editingMode === "drawing" ? "contained" : "outlined"}
+        >
+          <Icon>create</Icon>
+        </Button>
+        <Autocomplete
+          className="w-36"
+          value={selectedColor}
+          disabled={editingMode !== "drawing"}
+          onChange={(event, newValue) => setSelectedColor(newValue)}
+          options={colors}
+          getOptionLabel={(option) => option.label}
+          renderInput={(params) => (
+            <TextField {...params} label="Color del lapiz" />
+          )}
+        />
+        <Autocomplete
+          className="w-40"
+          value={selectedBrushSize}
+          disabled={editingMode !== "drawing"}
+          onChange={(event, newValue) => setSelectedBrushSize(newValue)}
+          options={brushSizes}
+          getOptionLabel={(option) => option.label}
+          renderInput={(params) => (
+            <TextField {...params} label="Tamaño de lapiz" />
+          )}
+        />
+        <Box flexGrow={1} />
+        <Button
+          onClick={() => setEditingMode("deleting")}
+          variant={editingMode === "deleting" ? "contained" : "outlined"}
         >
           <Icon>delete</Icon>
         </Button>
       </div>
       <OdontogramaCanvas
-        onClick={handleCanvasClick}
         placedSymbols={placedSymbols}
+        drawings={drawings}
+        editingMode={editingMode}
+        mousePosition={mousePosition}
+        onClick={handleCanvasClick}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
       />
     </div>
   );
