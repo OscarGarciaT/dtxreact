91bc23f7697d4370ace9cbbb282a347612645752
SebastRivera <sebluriv@espol.edu.ec>
2023-08-16 20:05:43 -0500

    Mochawesome conf- automatic reports->JSON,HTML,PNGS,VIDEOS

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "cypress";
-
 export default defineConfig({
+  reporter: "cypress-mochawesome-reporter", 
+  reporterOptions: {
+    "reportDir": "cypress/results",
+    "overwrite": false,
+    "html": true,
+    "json": true
+ },
   e2e: {
     specPattern: "./cypress/integration/*.spec.js",
     baseUrl: 'https://dentelx.vercel.app/login',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,28 +1,59 @@
-describe('Login and API Requests', () => {
-    it('Allows a user to log in and access protected API routes', () => {
-      cy.visit('/login');
-  
-      // Simular el login exitoso
-      cy.get('[name="email"]').type('oscar@dentelx.com');
-      cy.get('[name="password"]').type('abc123');
-      cy.get('.MuiButton-containedPrimary').click();
-      //cy.contains('Login Exitoso');
-      cy.wait(3000)
-      // Obtener el token de la respuesta del login
-      cy.getCookie('tokenDentelX').then((tokenCookie) => {
-        const token = tokenCookie.value;
-  
-        // Realizar solicitudes a la API con el token
-        cy.request({
-          method: 'GET',
-          url: 'https://dtxbackend.onrender.com/api/patients/649be3a8684dc54c7b7ba6db?searchQuery=',
-          headers: {
-            Authorization: `Bearer ${token}` // Incluir el token en las cabeceras
-          }
-        }).then((response) => {
-          // Aquí puedes realizar verificaciones en la respuesta de la API
-          // ...
-        });
-      });
+describe('Login and Sign Up', () => {
+  it('Allows a user to log in', () => {
+    cy.visit('/login'); // Asegúrate de ajustar la ruta si es necesario
+
+    cy.get('[name="email"]').type('oscar@dentelx.com');
+    cy.get('[name="password"]').type('abc123');
+    cy.get('.MuiButton-containedPrimary') // Selecciona el botón por la clase 'MuiButton-containedPrimary'
+      .should('have.text', 'Login in')  // Verifica el texto del botón si es necesario
+      .click();  
+    cy.intercept('GET', 'https://dtxbackend.onrender.com/api/patients/649be3a8684dc54c7b7ba6db?searchQuery=', {
+      statusCode: 200,
+      body: {} // Respuesta simulada
     });
+    cy.contains('Pacientes'); // Verifica que se muestre el mensaje de éxito
+   
   });
+  
+  it('Allows a user to sign up as dentist', () => {
+      cy.visit('/login'); // Asegúrate de ajustar la ruta si es necesario
+      cy.get('.MuiLink-root ').click();
+      cy.contains('Registro');
+      cy.get('[name="nombres"]').type('Sara Gema');
+      cy.get('[name="apellidos"]').type('Gonzales Suarez');
+      cy.get('[name="email"]').type('saraGG2@gmail.com');//Change email when testing
+      cy.get('[name="password"]').type('SarahG123');
+      cy.get('#mui-component-select-cargo').click();
+      cy.get('[data-value="DENTISTA"]').type('{enter}'); 
+      cy.get('[name="telefono"]').type('0989705423');
+      cy.get('.MuiButton-containedPrimary').click();
+      cy.wait(4000)
+      cy.contains('Pacientes');
+      cy.intercept('POST', 'https://dtxbackend.onrender.com/api/users/signup', {
+      statusCode: 200,
+      body: {} // Respuesta simulada
+      });
+
+    });
+
+    it('Allows a user to sign up as Ortod.', () => {
+      cy.visit('/login'); // Asegúrate de ajustar la ruta si es necesario
+      cy.get('.MuiLink-root ').click();
+      cy.contains('Registro');
+      cy.get('[name="nombres"]').type('Sara Gema');
+      cy.get('[name="apellidos"]').type('Gonzales Suarez');
+      cy.get('[name="email"]').type('saraGG3@gmail.com');//Change email when testing
+      cy.get('[name="password"]').type('SarahG123');
+      cy.get('#mui-component-select-cargo').click();
+      cy.get('[data-value="DENTISTA"]').type('{downarrow}{enter}'); 
+      cy.get('[name="telefono"]').type('0989705423');
+      cy.get('.MuiButton-containedPrimary').click();
+      cy.contains('Pacientes');
+      cy.intercept('POST', 'https://dtxbackend.onrender.com/api/users/signup', {
+      statusCode: 200,
+      body: {} // Respuesta simulada
+      });
+
+    });
+  
+});
